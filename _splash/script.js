7ae594faa2a2f01c27c26404c59e5425ef773329
Ryan Borgstrom <Rkborgstrom@mix.wvu.edu>
2018-05-31 14:56:57 -0500

    updated js

--- _splash/script.js
+++ _splash/script.js
@@ -9,8 +9,21 @@
     let tempMax_widget = document.getElementById('tempMax_widget');
     let tempMin_widget = document.getElementById('tempMin_widget');
     let weatherContainer = document.getElementById("weather-container");
+    let description_widget = document.getElementById('description_widget');
+    let icon_widget = document.getElementById('icon_widget');
+    let main_widget = document.getElementById('main_widget');
+    let name_widget = document.getElementById('name_widget');
     const weather = [];
     // let weatherWidget = document.getElementById('weather-widget')
+
+
+searchButton.addEventListener('keyup', function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {  //13 is the enter key
+        document.getElementById(searchButton).click();
+    }
+});
+
 
     searchButton.addEventListener('click', function (event) {
         if (search.value === "") {
@@ -22,26 +35,41 @@
                 .then(data => {
                     console.log(data);
 
-                    
+
                     let humidity = data.main.humidity;
                     let pressure = data.main.pressure;
                     let temp = data.main.temp;
                     let tempMax = data.main.temp_max;
                     let tempMin = data.main.temp_min;
+                    let description = data.weather[0].description;
+                    let icon = data.weather[0].icon;
+                    let main = data.weather[0].main;
+                    let name = data.name;
+
+
+
                     humidity_widget.setAttribute('value', humidity);
                     pressure_widget.setAttribute('value', pressure);
                     temp_widget.setAttribute('value', temp);
                     tempMax_widget.setAttribute('value', tempMax);
                     tempMin_widget.setAttribute('value', tempMin);
+                    icon_widget.setAttribute('value', icon);
+                    description_widget.setAttribute('value', description);
+                    main_widget.setAttribute('value', main);
+                    name_widget.setAttribute('value', name);
 
-                   
-                   weatherContainer.innerHTML = (`${temp}&#8457;, ${tempMax}&#8457;, ${tempMin}&#8457;, ${humidity}&#8457;`);
-             
-                   
-                //    weatherContainer.innerHTML = tempMax;
-                //    weatherContainer.innerHTML = tempMin;
-                   
 
+                    name_widget.innerHTML = (`${name}`);
+                    temp_widget.innerHTML = (` Current Temperature: ${temp}&#8457`);
+                    tempMax_widget.innerHTML = (` Day High: ${tempMax}&#8457;`);  
+                    tempMin_widget.innerHTML = (` Day Low: ${tempMin}&#8457;`);
+                    humidity_widget.innerHTML = (` Humidity: ${humidity}&#37;`);
+                    icon_widget.innerHTML = (`${icon}`);
+                    description_widget.innerHTML = (`${description}`);
+                    // main_widget.innerHTML = (`${main}`);
+
+
+            
 
                 })
                 .catch(error => console.log(error));
