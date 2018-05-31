060c28ebd909de21feb5b2ffe5f90a54c4e165ab
Ryan Borgstrom <Rkborgstrom@mix.wvu.edu>
2018-05-31 10:12:51 -0500

    updated js

--- _splash/script.js
+++ _splash/script.js
@@ -1,83 +1,48 @@
-// let APPID="48bec6862cf9f2f37892991bd1510a91";
-// let temp;
-// let location;
-// let icon;
-// let humidity;
-// let wind;
-// let direction;
-
-// function update(weather) {
-//     wind.innerHTML = weather.wind;
-//     direction.innerHTML = weather.direction;
-//     humidity.innerHTML = weather.humidity;
-//     location.innerHTML = weather.location;
-//     temp.innerHTML = weather.temp;
-//     icon.src = "imgs/codes/" + weather.icon + ".png";
-//     console.log(icon.src);
-
-// }
-
-// windown.onlaod = function() {
-//     temp = document.getElementById("temperature");
-//     location = document.getElementById("location");
-//     icon = document.getElementById("icon");
-//     humidity = document.getElementById("humidity");
-//     wind = document.getElementById("wind");
-//     direction = document.getElementById("direction");
-
-//     let weater = {};
-//     weather.wind = 3.5;
-//     weather.diretion = "N";
-//     weather.humidity = 75;
-//     weather.location = "Austin";
-//     weather.temp = "96";
-//     weather.icon = 200;
-
-
-
-//     update(weather);
-// }
-
-// let slider = document.getElementById('test-slider');
-// noUiSlider.create(slider, {
-//  start: [20, 80],
-//  connect: true,
-//  step: 1,
-//  orientation: 'horizontal', // 'horizontal' or 'vertical'
-//  range: {
-//    'min': 0,
-//    'max': 100
-//  },
-//  format: wNumb({
-//    decimals: 0
-//  })
-// });
-
-
 (function () {
 
     let API_KEY = "48bec6862cf9f2f37892991bd1510a91";
     let searchButton = document.getElementById('searchButton');
     let search = document.getElementById('search');
+    let humidity_widget = document.getElementById('humidity_widget');
+    let pressure_widget = document.getElementById('pressure_widget');
+    let temp_widget = document.getElementById('temp_widget');
+    let tempMax_widget = document.getElementById('tempMax_widget');
+    let tempMin_widget = document.getElementById('tempMin_widget');
+    let weatherContainer = document.getElementById("weather-container");
     const weather = [];
+    // let weatherWidget = document.getElementById('weather-widget')
 
     searchButton.addEventListener('click', function (event) {
         if (search.value === "") {
             alert('Please enter city')
         } else {
             event.preventDefault();
-            fetch(`http://api.openweathermap.org/data/2.5/weather?q=${search.value}&APPID=48bec6862cf9f2f37892991bd1510a91`)
+            fetch(`http://api.openweathermap.org/data/2.5/weather?q=${search.value}&units=imperial&APPID=48bec6862cf9f2f37892991bd1510a91`)
                 .then(response => response.json())
                 .then(data => {
                     console.log(data);
-                    let weatherArray = data.message;
+
+                    
                     let humidity = data.main.humidity;
                     let pressure = data.main.pressure;
                     let temp = data.main.temp;
-                    let tempMax = data.main.temp_Max;
-                    let tempMin = data.main.temp_Min;
-                 
-                    
+                    let tempMax = data.main.temp_max;
+                    let tempMin = data.main.temp_min;
+                    humidity_widget.setAttribute('value', humidity);
+                    pressure_widget.setAttribute('value', pressure);
+                    temp_widget.setAttribute('value', temp);
+                    tempMax_widget.setAttribute('value', tempMax);
+                    tempMin_widget.setAttribute('value', tempMin);
+
+                   
+                   weatherContainer.innerHTML = (`${temp}&#8457;, ${tempMax}&#8457;, ${tempMin}&#8457;, ${humidity}&#8457;`);
+             
+                   
+                //    weatherContainer.innerHTML = tempMax;
+                //    weatherContainer.innerHTML = tempMin;
+                   
+
+
                 })
                 .catch(error => console.log(error));
         }
